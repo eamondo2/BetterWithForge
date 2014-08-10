f4808e876924ec6cb059b39a2f582a2771f2b939
eamondo2 <eamonbb@gmail.com>
2014-08-10 00:36:29 -0400

    Updated modplan.

--- src/main/java/com/eamondo2/bwf/blocks/BWFBlockMillStone.java
+++ src/main/java/com/eamondo2/bwf/blocks/BWFBlockMillStone.java
@@ -1,11 +1,13 @@
 package com.eamondo2.bwf.blocks;
 
+import net.minecraft.block.material.Material;
+
 /**
  * Created by eamon_000 on 8/9/2014.
  */
 public class BWFBlockMillStone extends BWFBlock {
-	public BWFBlockMillStone() {
-
+	public BWFBlockMillStone(Material material) {
+        super(material);
 	}
 
 
